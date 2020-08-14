3cc7e2d3d02e0cac9e970a47a84e835c4de2baf3
Chhaian Pin <chhaianpin@hotmail.com>
2020-08-14 10:03:40 -0700

    added @hapi/joi library for validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+// Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async (req, res) => {
+
+    // Let's validate the data before we make a user
+    const {error , value} = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
