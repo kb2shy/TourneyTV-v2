6607e369d4420269e3aa394e70f5bcf6b4ca71df
Chhaian Pin <chhaianpin@hotmail.com>
2020-08-14 11:01:35 -0700

    added login route and validation to auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../model/User');
 const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation');
 
+// Register route
 router.post('/register', async (req, res) => {
 
     // Let's validate the data before we make a user
@@ -24,14 +25,32 @@ router.post('/register', async (req, res) => {
         password: hashedPassword
     });
 
-
     // Save user to database
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: savedUser._id });
     } catch (error) {
         res.status(400).send(error)
     }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+    // Validate the data the user used to login
+    const { error, value } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if email exists in db
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid login.");
+
+    // Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Invalid login");
+
+    // Password is correct
+    res.send("Login in success!");
+
 })
 
 module.exports = router;
