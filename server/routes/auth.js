00a4f7c760d79fe8e3249d14013c37c83587e2e1
Chhaian Pin <chhaianpin@hotmail.com>
2020-08-14 10:41:53 -0700

    added bcrypt library. passwords are now salted and hashed when user record is saved.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -8,12 +9,23 @@ router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    // Checking if user email is already in the database
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) return res.status(400).send("Email already exists");
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
+
+    // Save user to database
     try {
         const savedUser = await user.save();
         res.send(savedUser);
