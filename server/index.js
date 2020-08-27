10d346c3c85325fc91db00fb5593eae63ea5394d
Chhaian Pin <chhaianpin@hotmail.com>
2020-08-26 20:38:58 -0700

    added chatroom and message routes to index/server file.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 // import routes
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const chatroomRoute = require('./routes/chatroomRoute');
+const messageRoute = require('./routes/messageRoute');
 
 // Connect to DB
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
@@ -18,6 +20,8 @@ app.use(express.json());
 // route middlewares
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/chatrooms', chatroomRoute);
+app.use('/api/messages', messageRoute);
 
 const PORT = process.env.PORT || 5000;
 
